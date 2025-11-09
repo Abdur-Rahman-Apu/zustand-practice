52796b5387f1eb95ffa3d05bbb107bb18d42a211
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2025-11-09 21:27:45 +0600

    Immer used for nested object update

--- src/store/bearStore.tsx
+++ src/store/bearStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 type IBearStore = {
   bears: {
@@ -11,21 +12,25 @@ type IBearStore = {
   summary: () => string;
 };
 
-export const useBearStore = create<IBearStore>()((set, get) => ({
-  bears: {
-    small: 0,
-    big: 0,
-  },
-  incrementBigBear: () =>
-    set((state) => ({ bears: { ...state.bears, big: state.bears.big + 1 } })),
-  incrementSmallBear: () =>
-    set((state) => ({
-      bears: { ...state.bears, small: state.bears.small + 1 },
-    })),
-  summary: () => {
-    const smallBears = get().bears.small;
-    const bigBears = get().bears.big;
-    const total = smallBears + bigBears;
-    return `There are total ${total} bears`;
-  },
-}));
+export const useBearStore = create<IBearStore>()(
+  immer((set, get) => ({
+    bears: {
+      small: 0,
+      big: 0,
+    },
+    incrementBigBear: () =>
+      set((state) => {
+        state.bears.big++;
+      }),
+    incrementSmallBear: () =>
+      set((state) => {
+        state.bears.small++;
+      }),
+    summary: () => {
+      const smallBears = get().bears.small;
+      const bigBears = get().bears.big;
+      const total = smallBears + bigBears;
+      return `There are total ${total} bears`;
+    },
+  }))
+);
