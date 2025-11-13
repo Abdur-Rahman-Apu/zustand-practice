ea42256038c6666ec7b0b2b0ef3e359aab9e75ed
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2025-11-13 20:55:18 +0600

    Reset memory

--- src/components/Fish/Fish.tsx
+++ src/components/Fish/Fish.tsx
@@ -9,7 +9,7 @@ const Fish = () => {
       <p>{Math.random()}</p>
       <button onClick={incrementSmallFish}>Add Small Fish</button>
       <button onClick={useFishStore.persist.clearStorage}>Clear Storage</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={reset}>Reset Memory</button>
     </div>
   );
 };
