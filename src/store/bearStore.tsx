5497349af48dde09878efbc0f2da468866f2050c
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2025-11-14 20:51:01 +0600

    Slice example

--- src/store/bearStore.tsx
+++ src/store/bearStore.tsx
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import { create, type StateCreator } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 type IBearStore = {
@@ -12,25 +12,26 @@ type IBearStore = {
   summary: () => string;
 };
 
+const createBearSlice:StateCreator<IBearStore,[["zustand/immer", never]]> = (set, get) => ({
+  bears: {
+    small: 0,
+    big: 0,
+  },
+  incrementBigBear: () => set((state) => {
+    state.bears.big++;
+  }),
+  incrementSmallBear: () => set((state) => {
+    state.bears.small++;
+  }),
+  summary: () => {
+    const smallBears = get().bears.small;
+    const bigBears = get().bears.big;
+    const total = smallBears + bigBears;
+    return `There are total ${total} bears`;
+  },
+});
+
+
 export const useBearStore = create<IBearStore>()(
-  immer((set, get) => ({
-    bears: {
-      small: 0,
-      big: 0,
-    },
-    incrementBigBear: () =>
-      set((state) => {
-        state.bears.big++;
-      }),
-    incrementSmallBear: () =>
-      set((state) => {
-        state.bears.small++;
-      }),
-    summary: () => {
-      const smallBears = get().bears.small;
-      const bigBears = get().bears.big;
-      const total = smallBears + bigBears;
-      return `There are total ${total} bears`;
-    },
-  }))
+  immer(createBearSlice)
 );
