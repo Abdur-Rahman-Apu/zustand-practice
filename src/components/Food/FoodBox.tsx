d1c34e393b9c707e6c0a9d64bfc3333cf79c2d15
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2025-11-14 20:34:13 +0600

    Get, set state example

--- src/components/Food/FoodBox.tsx
+++ src/components/Food/FoodBox.tsx
@@ -5,14 +5,21 @@ const FoodBox = () => {
     const addOneFish=useFoodStore((state)=>state.addOneFish)
     const removeOneFish=useFoodStore((state)=>state.removeOneFish)
     const removeAllFish=useFoodStore((state)=>state.removeAllFish)
+
+    const addFiveFish=()=>{
+        useFoodStore.setState((state)=>({fish:state.fish+5}))
+    }
+
+    const nonReactiveFish=useFoodStore.getState().fish;
   return (
     <div>
         <h1>Food Box</h1>
         <h2>Fish: {fish}</h2>
+        <h2>Non Reactive Fish: {nonReactiveFish}</h2>
         <button onClick={addOneFish} style={{margin:"3px"}}>Add One Fish</button>
         <button onClick={removeOneFish} style={{margin:"3px"}}>Remove One Fish</button>
         <button onClick={removeAllFish} style={{margin:"3px"}}>Remove All Fish</button>
-
+        <button onClick={addFiveFish} style={{margin:"3px"}}>Add Five Fish</button>
     </div>
 
   )
